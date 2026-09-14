81f35171a432b7b43cb375cc23a94004cf6f5572
vaibhavbora11124 <Vaibhav.33200@stu.upes.ac.in>
2026-09-14 14:35:41 +0530

    Add files via upload

---
+++ q71.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols;
+    int matrix[100][100];
+    int i, j;
+
+    scanf("%d %d", &rows, &cols);
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
