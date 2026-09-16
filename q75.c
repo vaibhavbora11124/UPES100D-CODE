e92607e0b0338a653ad9b7f25b2b21e485a77b62
Vaibhav Bora <vaibhavbora@Vaibhavs-MacBook-Air-3.local>
2026-09-16 20:00:00 +0530

    Add q75 program

---
+++ q75.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int a[100][100], b[100][100], sum[100][100];
+
+    scanf("%d %d", &r, &c);
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    scanf("%d %d", &r, &c);
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
