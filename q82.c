20552fd67739204efeab619a89e769342b43768b
vaibhavbora11124 <Vaibhav.33200@stu.upes.ac.in>
2026-09-19 11:32:27 +0530

    Add files via upload

---
+++ q82.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+
+    return 0;
+}
